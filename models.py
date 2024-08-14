532897eba4a486e8f060a7705d99b16b36256c78
Nik Kor <3357381Nk@gmail.com>
2024-08-14 22:13:15 +0100

    seeding files done

--- models.py
+++ models.py
@@ -1,10 +1,5 @@
 from mongoengine import Document
-from mongoengine.fields import (
-    DateTimeField,
-    ListField,
-    StringField,
-    ReferenceField
-)
+from mongoengine.fields import DateTimeField, ListField, StringField, ReferenceField
 
 
 class authors(Document):
@@ -12,10 +7,9 @@ class authors(Document):
     born_date = DateTimeField(required=True)
     born_location = StringField(required=True)
     description = StringField(required=True)
-    
+
+
 class qoutes(Document):
     tags = ListField(StringField())
     author = ReferenceField(authors)
-    qoute = StringField(required=True)
-    
-
+    quote = StringField(required=True)
--- seeds.py
+++ seeds.py
@@ -1,7 +1,9 @@
 from mongoengine import connect
 import configparser
+from models import authors, qoutes
+import json
 
-#read config file for DB key safety
+# read config file for DB key safety
 config = configparser.ConfigParser()
 config.read("configs/config.ini")
 
@@ -10,13 +12,48 @@ mongodb_pass = config.get("DB", "pass")
 domain = config.get("DB", "domain")
 
 
+# reading and parsing json file for authors and saving to DB
+def seed_authors():
+    with open("contents/authors.json") as f:
+        data = json.load(f)
+        for author in data:
+            author = authors(
+                full_name=author["fullname"],
+                born_date=author["born_date"],
+                born_location=author["born_location"],
+                description=author["description"],
+            )
+            author.save()
+
+
+# same for quotes
+def seed_qoutes():
+    with open("contents/qoutes.json") as f:
+        data = json.load(f)
+        for quote in data:
+            quote = qoutes(
+                tags=quote["tags"],
+                author=authors.objects(full_name=quote["author"]).first(),
+                quote=quote["quote"],
+            )
+            quote.save()
+
+
 if __name__ == "__main__":
-    #check connection to DB
+    # check connection to DB
     try:
         connect(
             host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/?retryWrites=true&w=majority""",
             ssl=True,
         )
         print("Connected to DB")
+    except Exception as e:
+        print("Error: ", e)
+
+    # seed authors and qoutes
+    try:
+        seed_authors()
+        seed_qoutes()
+        print("Seeding completed!")
     except Exception as e:
         print("Error: ", e)
