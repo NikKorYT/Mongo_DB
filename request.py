67173a8c612cc1e48696c434adfdd144993ceae2
Nik Kor <3357381Nk@gmail.com>
2024-08-14 22:38:29 +0100

    added request file

---
+++ request.py
@@ -0,0 +1,7 @@
+#Script to search for citations by tag, author name, or a set of tags. The script is executed in an infinite loop and uses the usual input statement to accept commands in the following command:value format. Example:
+# name: Steve Martin - find and return a list of all quotes by the author Steve Martin;
+# tag:life - find and return a list of quotes for the tag life;
+# tags:life,live - find and return a list of quotes that contain the life or live tags (note: no spaces between the life, live tags);
+# exit - exit the script;
+
+
